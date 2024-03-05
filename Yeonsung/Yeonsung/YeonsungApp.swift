c2aa2cf4846473a647c2c979259a908f5bb640ea
YangMun <53438709+YangMun@users.noreply.github.com>
2024-03-05 14:28:32 +0900

    최종 결과물

---
+++ Yeonsung/Yeonsung/YeonsungApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeonsungApp.swift
+//  Yeonsung
+//
+//  Created by 양문경 on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct YeonsungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
